aa3f1786315bf5b67e5b45e1ce92151fe1314f1e
Joe Mullen <mullenjoe13@gmail.com>
2018-10-28 17:39:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-var addElementToBeginningOfArray = function(element, array) { array.push(element)
+var addElementToBeginningOfArray = function(array, element) { array.push(element)
 return array
 }
 
